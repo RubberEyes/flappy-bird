97a307b8728a5370c8fbe017b8e66e021740e897
RubberEyes <ninovello2000@gmail.com>
2026-02-01 14:35:54 -0300

    It is complete

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ use bevy::prelude::{App, AppExit, DefaultPlugins};
 use flappy_lib::plugins::*;
 fn main() -> AppExit {
     let mut app = App::new();
-    app.add_plugins((DefaultPlugins, CameraPlugin, PlayerPlugin, PipePlugin));
+    app.add_plugins((
+        DefaultPlugins,
+        CameraPlugin,
+        ScorePlugin,
+        PlayerPlugin,
+        PipePlugin,
+        BackgroundPlugin,
+    ));
     app.run()
 }
